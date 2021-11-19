a915965c859694981741f72836bbbed64dea7a7d
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2021-11-19 05:16:35 +0100

    add integration tests

---
+++ tests/integration_test_03.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+source tests/assert.sh
+test_data=vafator/tests/resources
+output=output/integration_test_03
+input_vcf=$test_data/project.NIST.hc.snps.indels.chr1_1000000_2000000.vcf
+output_vcf=$output/vafator.vcf
+mkdir -p $output
+vafator --input-vcf $input_vcf \
+--output-vcf $output_vcf \
+--bam my_normal $test_data/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.chr1_1000000_2000000.bam \
+--bam my_tumor $test_data/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.chr1_1000000_2000000.bam \
+--mapping-quality 40 --base-call-quality 40
+
+test -s $output/vafator.vcf || { echo "Missing VCF output file!"; exit 1; }
+assert_eq `cat $output_vcf | grep -v '#' | grep my_normal_af | wc -l` `cat $input_vcf | grep -v '#' | wc -l` "Wrong number of variants"
+assert_eq `cat $output_vcf | grep -v '#' | grep my_tumor_af | wc -l` `cat $input_vcf | grep -v '#' | wc -l` "Wrong number of variants"
+assert_eq `cat $output_vcf | grep -v '#' | grep my_normal_ac | wc -l` `cat $input_vcf | grep -v '#' | wc -l` "Wrong number of variants"
+assert_eq `cat $output_vcf | grep -v '#' | grep my_tumor_ac | wc -l` `cat $input_vcf | grep -v '#' | wc -l` "Wrong number of variants"
+assert_eq `cat $output_vcf | grep -v '#' | grep my_normal_dp | wc -l` `cat $input_vcf | grep -v '#' | wc -l` "Wrong number of variants"
+assert_eq `cat $output_vcf | grep -v '#' | grep my_tumor_dp | wc -l` `cat $input_vcf | grep -v '#' | wc -l` "Wrong number of variants"
