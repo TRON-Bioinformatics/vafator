a4280d0417808c0f4abaa197c737976a6dfc3aad
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2021-12-09 21:57:25 +0100

    fix some issues in genedator

--- vafator/genedator.py
+++ vafator/genedator.py
@@ -5,25 +5,32 @@ import csv
 import argparse
 from logzero import logger
 import sys
+import gzip
 
 
 DEFAULT_BIOTYPES = ["protein_coding", "IG_V_gene", "IG_C_gene", "IG_J_gene", "IG_D_gene",
                     "TR_C_gene", "TR_J_gene", "TR_V_gene", "TR_D_gene", ]
-BATCH_SIZE = 10000
+BATCH_SIZE = 100000
 
 
 class Genedator:
 
-    def __init__(self, gff_file, bam_file, output_csv_file,  biotypes=DEFAULT_BIOTYPES):
-        gtf = gtfparse.read_gtf(gff_file)
+    def __init__(self, gtf_file, bam_file, output_csv_file, biotypes=DEFAULT_BIOTYPES):
+        logger.info("Reading GTF file...")
+        gtf = gtfparse.read_gtf(gtf_file)
         self.genes = gtf[(gtf.feature == "gene") & (gtf.gene_type.isin(biotypes))]
         self.bam = pysam.AlignmentFile(bam_file, "rb")
-        self.fd = open(output_csv_file, 'w', newline='')
+        self.fd = gzip.open(output_csv_file, 'w')
         self.writer = csv.writer(self.fd, delimiter='\t')
 
     def run(self):
         data = []
         counter = 0
+        # write header
+        self.writer.writerow((
+            "chromosome", "gene_name", "read_name",
+            "read_length", "cigar", "flag", "insertion_size", "mapping_quality",
+            "median_bcq", "max_bcq", "min_bcq"))
         for i, g in self.genes.iterrows():
             chromosome = g.seqname
             gene_name = g.gene_name
@@ -38,7 +45,7 @@ class Genedator:
                                  np.median(read.query_qualities),
                                  np.max(read.query_qualities),
                                  np.min(read.query_qualities)))
-                    if len(data) > BATCH_SIZE:
+                    if len(data) >= BATCH_SIZE:
                         self.writer.writerows(data)
                         counter = counter + len(data)
                         data = []
@@ -52,8 +59,8 @@ if __name__ == '__main__':
         description="genedator", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--input-bam", dest="input_bam", action="store", help="The BAM file to annotate with", required=True)
     parser.add_argument("--output-csv", dest="output_csv", action="store", help="The annotations CSV", required=True)
-    parser.add_argument("--gff", dest="gff", action="store",
-                        help='The GFF file defining the genes to annotate', default='tumor')
+    parser.add_argument("--gtf", dest="gtf", action="store",
+                        help='The GTF file defining the genes to annotate', default='tumor')
     args = parser.parse_args()
 
     try:
@@ -61,7 +68,7 @@ if __name__ == '__main__':
         Genedator(
             bam_file=args.input_bam,
             output_csv_file=args.output_csv,
-            gff_file=args.gff
+            gtf_file=args.gtf
         ).run()
     except Exception as e:
         logger.exception(e)
