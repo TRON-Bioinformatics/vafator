0dfbb3c0074d327d12509a98cf9e63f563ce5760
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-01-28 12:26:14 +0100

    draft version of genedator

--- vafator/genedator.py
+++ vafator/genedator.py
@@ -20,36 +20,102 @@ class Genedator:
         gtf = gtfparse.read_gtf(gtf_file)
         self.genes = gtf[(gtf.feature == "gene") & (gtf.gene_type.isin(biotypes))]
         self.bam = pysam.AlignmentFile(bam_file, "rb")
-        self.fd = gzip.open(output_csv_file, 'w')
+        self.fd = gzip.open(output_csv_file, 'wt')
         self.writer = csv.writer(self.fd, delimiter='\t')
 
     def run(self):
-        data = []
-        counter = 0
         # write header
         self.writer.writerow((
-            "chromosome", "gene_name", "read_name",
-            "read_length", "cigar", "flag", "insertion_size", "mapping_quality",
-            "median_bcq", "max_bcq", "min_bcq"))
+            "chromosome", "gene_name",
+            "read_length_pct25", "read_length_pct50", "read_length_pct75",
+            "mapping_quality_pct25", "mapping_quality_pct50", "mapping_quality_pct75",
+            "insert_size_pct25", "insert_size_pct50", "insert_size_pct75",
+            "median_bcq_pct25", "median_bcq_pct25", "median_bcq_pct25",
+            "count_reads",
+            "count_paired",
+            "count_proper_pair",
+            "count_qcfail",
+            "count_read1",
+            "count_read2",
+            "count_reverse",
+            "count_secondary",
+            "count_supplementary",
+            "count_duplicates"))
+        num_genes = self.genes.shape[0]
+        count_genes = 0
         for i, g in self.genes.iterrows():
             chromosome = g.seqname
             gene_name = g.gene_name
+            read_lengths = []
+            mqs = []
+            insert_sizes = []
+            median_bcq = []
+            count_reads = 0
+            count_duplicates = 0
+            count_paired = 0
+            count_proper_pair = 0
+            count_qcfail = 0
+            count_read1 = 0
+            count_read2 = 0
+            count_reverse = 0
+            count_secondary = 0
+            count_supplementary = 0
             for read in self.bam.fetch(chromosome, g.start, g.end):
+
                 if not read.is_duplicate:
-                    data.append((chromosome, gene_name, read.query_name,
-                                 read.alen,
-                                 read.cigarstring,
-                                 read.flag,
-                                 read.isize,
-                                 read.mapping_quality,
-                                 np.median(read.query_qualities),
-                                 np.max(read.query_qualities),
-                                 np.min(read.query_qualities)))
-                    if len(data) >= BATCH_SIZE:
-                        self.writer.writerows(data)
-                        counter = counter + len(data)
-                        data = []
-                        logger.info("Wrote {} reads".format(counter))
+                    read_lengths.append(read.rlen)
+                    mqs.append(read.mapping_quality)
+                    insert_sizes.append(read.isize)
+                    median_bcq.append(np.median(read.query_qualities))
+                    count_reads += 1
+                    if read.is_paired:
+                        count_paired += 1
+                    if read.is_proper_pair:
+                        count_proper_pair += 1
+                    if read.is_qcfail:
+                        count_qcfail += 1
+                    if read.is_read1:
+                        count_read1 += 1
+                    if read.is_read2:
+                        count_read2 += 1
+                    if read.is_reverse:
+                        count_reverse += 1
+                    if read.is_secondary:
+                        count_secondary += 1
+                    if read.is_supplementary:
+                        count_supplementary += 1
+
+                else:
+                    count_duplicates += 1
+
+            self.writer.writerow((
+                chromosome, gene_name,
+                np.percentile(read_lengths, 0.25),
+                np.median(read_lengths),
+                np.percentile(read_lengths, 0.75),
+                np.percentile(mqs, 0.25),
+                np.median(mqs),
+                np.percentile(mqs, 0.75),
+                np.percentile(insert_sizes, 0.25),
+                np.median(insert_sizes),
+                np.percentile(insert_sizes, 0.75),
+                np.percentile(median_bcq, 0.25),
+                np.median(median_bcq),
+                np.percentile(median_bcq, 0.75),
+                count_reads,
+                count_paired,
+                count_proper_pair,
+                count_qcfail,
+                count_read1,
+                count_read2,
+                count_reverse,
+                count_secondary,
+                count_supplementary,
+                count_duplicates
+              ))
+            count_genes += 1
+            if count_genes % 100 == 0:
+                logger.info("Annotated {} % of genes".format(round(float(count_genes)/num_genes, 3)))
         self.fd.close()
 
 
