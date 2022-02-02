e39129b06291c46342fb0779bee8f1746cca4893
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-02-02 14:36:28 +0100

    use counters and reduce operations in loops to improve performance

--- vafator/pileups.py
+++ vafator/pileups.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from dataclasses import dataclass
 from typing import List
 from cyvcf2 import Variant
@@ -36,7 +37,9 @@ def get_variant_pileup(
     position = variant.position
     # this function returns the pileups at all positions covered by reads covered the queried position
     # approximately +- read size bp
-    return bam.pileup(contig=chromosome, start=position - 1, stop=position, truncate=True,
+    return bam.pileup(contig=chromosome, start=position - 1, stop=position,
+                      truncate=True,                    # returns only this column
+                      max_depth=0,                      # disables maximum depth
                       min_base_quality=min_base_quality,
                       min_mapping_quality=min_mapping_quality)
 
@@ -49,7 +52,7 @@ class CoverageMetrics:
 
 def get_metrics(variant: VafatorVariant, pileups: IteratorColumnRegion) -> CoverageMetrics:
     if variant.is_snp():
-        return get_snv_metrics(variant, pileups)
+        return get_snv_metrics(pileups)
     elif variant.is_insertion():
         return get_insertion_metrics(variant, pileups)
     elif variant.is_deletion():
@@ -63,26 +66,26 @@ def get_insertion_metrics(variant: VafatorVariant, pileups: IteratorColumnRegion
     position = variant.position
     insertion_length = len(variant.alternative[0]) - len(variant.reference)
     insertion = variant.alternative[0][1:]
+    alt_upper = variant.alternative[0].upper()
     try:
-        pileup = next(pileups)
-        for r in pileup.pileups:
-            dp += 1
-            if r.indel > 0:
-                # read with an insertion
-                start = r.alignment.reference_start
-                relative_position = 0
-                for cigar_type, cigar_length in r.alignment.cigartuples:
-                    if cigar_type in [0, 2, 3, 7, 8]:  # consumes reference M, D, N, =, X
-                        start += cigar_length
-                        if start > position:
-                            break
-                    if cigar_type in [0, 1, 4, 7, 8]:  # consumes query M, I, S, =, X
-                        relative_position += cigar_length
-                    if cigar_type == 1:  # does not count I
-                        insertion_in_query = r.alignment.query[relative_position:relative_position + insertion_length]
-                        if start == position and cigar_length == insertion_length and \
-                                insertion == insertion_in_query:
-                            ac[variant.alternative[0].upper()] = ac[variant.alternative[0].upper()] + 1
+        pileups = [p for p in next(pileups).pileups if p.indel > 0]
+        dp += len(pileups)
+        for p in pileups:
+            # read with an insertion
+            start = p.alignment.reference_start
+            relative_position = 0
+            for cigar_type, cigar_length in p.alignment.cigartuples:
+                if cigar_type in [0, 2, 3, 7, 8]:  # consumes reference M, D, N, =, X
+                    start += cigar_length
+                    if start > position:
+                        break
+                if cigar_type in [0, 1, 4, 7, 8]:  # consumes query M, I, S, =, X
+                    relative_position += cigar_length
+                if cigar_type == 1:  # does not count I
+                    insertion_in_query = p.alignment.query[relative_position:relative_position + insertion_length]
+                    if start == position and cigar_length == insertion_length and \
+                            insertion == insertion_in_query:
+                        ac[alt_upper] = ac[alt_upper] + 1
     except StopIteration:
         # no reads
         pass
@@ -94,23 +97,23 @@ def get_deletion_metrics(variant: VafatorVariant, pileups: IteratorColumnRegion)
     dp = 0
     position = variant.position
     deletion_length = len(variant.reference) - len(variant.alternative[0])
+    alt_upper = variant.alternative[0].upper()
     try:
-        pileup = next(pileups)
-        for r in pileup.pileups:
-            dp += 1
-            if r.indel < 0:
-                # read with an deletion
-                start = r.alignment.reference_start
-                for cigar_type, cigar_length in r.alignment.cigartuples:
-                    if cigar_type in [0, 3, 7, 8]:  # consumes reference M, N, =, X
+        pileups = [p for p in next(pileups).pileups if p.indel < 0]
+        dp += len(pileups)
+        for r in pileups:
+            # read with a deletion
+            start = r.alignment.reference_start
+            for cigar_type, cigar_length in r.alignment.cigartuples:
+                if cigar_type in [0, 3, 7, 8]:  # consumes reference M, N, =, X
+                    start += cigar_length
+                elif cigar_type == 2:  # D
+                    if start == position and cigar_length == deletion_length:
+                        ac[alt_upper] = ac[alt_upper] + 1
+                    else:
                         start += cigar_length
-                    elif cigar_type == 2:  # D
-                        if start == position and cigar_length == deletion_length:
-                            ac[variant.alternative[0].upper()] = ac[variant.alternative[0].upper()] + 1
-                        else:
-                            start += cigar_length
-                    if start > position:
-                        break
+                if start > position:
+                    break
     except StopIteration:
         # no reads
         pass
@@ -123,17 +126,14 @@ def _initialize_empty_count(bases_counts, base):
     return bases_counts
 
 
-def get_snv_metrics(variant: VafatorVariant, pileups: IteratorColumnRegion) -> CoverageMetrics:
+def get_snv_metrics(pileups: IteratorColumnRegion) -> CoverageMetrics:
     dp = 0
-    ac = {alt.upper(): 0 for alt in variant.alternative}
-    ac[variant.reference.upper()] = 0
+    ac = Counter()
     try:
         pileup = next(pileups)
-        for s in pileup.get_query_sequences():
-            dp += 1
-            base_upper_case = s.upper()
-            if base_upper_case in ac:
-                ac[base_upper_case] = ac[base_upper_case] + 1
+        bases = [s.upper() for s in pileup.get_query_sequences()]
+        dp += len(bases)
+        ac = ac + Counter(bases)
     except StopIteration:
         # no reads
         pass
