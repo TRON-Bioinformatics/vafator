7863daf0b8ffedb0552566eaf20de436ee1a49a7
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-07-23 07:17:45 +0200

    implement the absolute power

--- vafator/tests/test_power_calculator.py
+++ vafator/tests/test_power_calculator.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+import numpy as np
 import pkg_resources
 
 from ploidies import PloidyManager
@@ -74,3 +75,19 @@ class PowerCalculatorTest(TestCase):
             power.calculate_power(dp=10, ac=2, sample='tumor',
                                   variant=VafatorVariant(
                                       chromosome='chr1', position=10002, reference='A', alternative='G')))
+
+    def test_absolute_power_calculator(self):
+        power = PowerCalculator(
+            tumor_ploidies={'tumor': PloidyManager(genome_wide_ploidy=2.5)}, purities={'tumor': 0.8})
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=0, sample='tumor', variant=None), 0.0)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=1, sample='tumor', variant=None), 0.9823689574968194)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=2, sample='tumor', variant=None), 0.8956871759303671)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=3, sample='tumor', variant=None), 0.7267089415880859)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=4, sample='tumor', variant=None), 26.106282321427628)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=5, sample='tumor', variant=None), 26389.64274484705)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=6, sample='tumor', variant=None), 28473705.99681597)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=7, sample='tumor', variant=None), np.inf)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=8, sample='tumor', variant=None), np.inf)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=9, sample='tumor', variant=None), np.inf)
+        self.assertEqual(power.calculate_absolute_power(dp=10, ac=10, sample='tumor', variant=None), np.inf)
+        self.assertTrue(np.isnan(power.calculate_absolute_power(dp=10, ac=11, sample='tumor', variant=None)))
